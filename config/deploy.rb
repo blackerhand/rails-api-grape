cf0d230f1ec8f3679a586240181b018ddcf083d4
black <black@paimingjia.com>
2023-04-14 09:37:24 +0800

    puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,11 +23,11 @@ set :deploy_to, "/var/www/grape_v2"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-set :linked_files, ['config/database.yml']
+set :linked_files, ['config/database.yml', 'config/credentials/staging.key', 'config/credentials/production.key']
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'config/credentials/staging.key', 'config/credentials/production.key']
+set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads']
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
---
+++ config/puma/staging.rb
@@ -0,0 +1,27 @@
+app_dir = File.expand_path("../../..", __FILE__)
+puts app_dir
+system 'mkdir -p tmp/sockets'
+system 'mkdir -p tmp/pids'
+
+_port = 9020
+
+puts "rails_env:" + ENV['RAILS_ENV'].to_s
+puts "port:" + _port.to_s
+port _port
+environment "staging"
+# directory '/data/xxxx'
+
+rackup "#{app_dir}/config.ru"
+
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
+stdout_redirect nil, "#{app_dir}/log/puma.stderr.log", true
+
+# daemonize true
+workers 2
+threads 4, 32
+
+preload_app!
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
