66f3b009354bd9ece9cda6dde815b10b92e61aec
black <black@paimingjia.com>
2023-04-14 09:42:13 +0800

    add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+  # Sidekiq::Web.use ActionDispatch::Cookies
+  # Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+  #
+  unless Rails.env.development?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      username == 'admin' && password == '7846e1b0119b18893j41151b280ecdf7'
+    end
+  end
+
+  mount Sidekiq::Web => '/sidekiq'
 end
