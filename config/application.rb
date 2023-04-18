b679cc491f0282f010f62590bc133955ffa16bab
black <black@paimingjia.com>
2023-04-18 11:37:18 +0800

    config autoload

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,8 @@ module RailsApiGrapeV2
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # config.eager_load_paths << Rails.root.join("app", 'api')
+    config.autoload_paths << Rails.root.join("app", 'api')
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
